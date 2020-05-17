b11d352aa9a78c9c2328102ae53c2a267ce64f98
nishidayoshikatsu <nsd24technology@gmail.com>
2020-05-17 21:47:21 +0900

    チャンネル一覧の作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app-layout">
     <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p>#general</p>
+      <p>#random</p>
+      <p>#random</p>
     </div>
     <div class="main-content">
       <nuxt />
@@ -17,6 +21,12 @@
     width: 300px;
     height: 100vh;
     background: #4A4141;
+    padding: 20px;
+  }
+
+  .sidebar p {
+    color: #DDDDDD;
+    padding-top: 4px;
   }
 
   .main-content {
