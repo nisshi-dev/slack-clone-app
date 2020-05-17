7dcef6bf39ef447a8c8a264c6e49a519ad248044
nishidayoshikatsu <nsd24technology@gmail.com>
2020-05-17 23:13:29 +0900

    eslintのエラーを修正

--- components/Message.vue
+++ components/Message.vue
@@ -24,8 +24,8 @@ export default {
     }
   },
   computed: {
-    displayName() {
-      return "@" + this.message.user.name
+    displayName () {
+      return `@${this.message.user.name}`
     }
   }
 }
