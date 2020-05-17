e9ca07316a3969705d98b605ec8a376885453106
nishidayoshikatsu <nsd24technology@gmail.com>
2020-05-17 22:02:49 +0900

    チャット画面の作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="container">
+    <div class="chats-container">
+      <div class="chat-container">
+        <div class="thumbnail-container">
+          <img src="https://ca.slack-edge.com/T080R6JAE-UFJ230JH0-5eed01d30f7d-512" />
+        </div>
+        <div class="message-container">
+          <div class="user-name">NSD</div>
+          <div class="message">今日も良い天気ですね</div>
+        </div>
+      </div>
+
+      <div class="chat-container">
+        <div class="thumbnail-container">
+          <img src="https://ca.slack-edge.com/T080R6JAE-UFJ230JH0-5eed01d30f7d-512" />
+        </div>
+        <div class="message-container">
+          <div class="user-name">NSD</div>
+          <div class="message">そうですねー</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,35 +32,27 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
+  .chats-container {
+    padding: 16px;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .chat-container {
+    display: flex;
+    padding: 8px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .thumbnail-container {
+    margin-right: 16px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .thumbnail-container img {
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+  }
+
+  .user-name {
+    font-weight: bold;
+  }
 </style>
